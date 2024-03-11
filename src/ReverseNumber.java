2ff75a606b95fc1dc21f200738a97e2da5fca2df
Anoop Vantagodi <anoopvantagodi420@gmail.com>
2024-03-11 22:43:47 +0530

    Reversing the given number

---
+++ src/ReverseNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args){
+        // Reverse the given number
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        // take input from user
+        int number = input.nextInt();
+        int reverse = 0;
+        while (number>0){
+            int rem = number%10;
+            reverse = reverse*10 + rem;
+            number = number/10;
+        }
+        System.out.println("Reversed number is: " + reverse);
+    }
+}
